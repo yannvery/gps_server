34503e31c99952e3a4a88c35d9f38aa44facdb53
Yann VERY <yann.very@gmail.com>
2019-04-22 12:45:15 +0200

    Fix: Heroku deployment doesn't need the prod.secret file

--- config/prod.exs
+++ config/prod.exs
@@ -74,4 +74,4 @@ config :hello, Hello.Repo,
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
