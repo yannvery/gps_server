a4c347cd696793daf000c2318e702c290b97acdd
Yann VERY <yann.very@gmail.com>
2019-04-22 13:23:24 +0200

    Fix: Add secret key base in production

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ config :gps_server, GpsServerWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "shrouded-earth-75690", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
