048ed78bdf357ecdb7142c12eacea5337ca385cd
Yann VERY <yann.very@gmail.com>
2019-04-22 13:17:17 +0200

    Fix: Repo used in production

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :gps_server, GpsServerWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :hello, Hello.Repo,
+config :gps_server, GpsServer.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
