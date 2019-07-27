6113d7a248773012e84369aa14e6896f4fefcc69
Yann VERY <yann.very@gmail.com>
2019-07-27 13:21:13 +0200

    Feat(path): naive implementation

--- lib/gps_server_web/controllers/page_controller.ex
+++ lib/gps_server_web/controllers/page_controller.ex
@@ -3,6 +3,19 @@ defmodule GpsServerWeb.PageController do
 
   def index(conn, _params) do
     position = GpsServer.last_position()
-    render(conn, "index.html", position: position)
+    path = get_path()
+    render(conn, "index.html", position: position, path: path)
+  end
+
+  def get_path() do
+    import Ecto.Query
+
+    query =
+      from p in GpsServer.Position,
+        where: p.inserted_at > type(^~N[2019-05-02 00:00:00], :naive_datetime),
+        order_by: [asc: p.id]
+
+    GpsServer.Repo.all(query)
+    |> Enum.map(fn x -> [x.latitude, x.longitude] end)
   end
 end
